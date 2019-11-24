c2a3518e2e069af64248b7a038694af7aa2de4b9
6Ipkti <boonyaporn.sudjunham@kkumail.com>
2019-11-24 14:15:27 +0700

    v1

--- js/microgear.js
+++ js/microgear.js
@@ -6,7 +6,7 @@
   const ALIAS = "DigitalOUTPUT_HTML_web";     //  ชื่อตัวเอง
   const thing1 = "NodeMCU1"; //  ชื่อเพื่อนที่จะคุย
 
-  var toggle = 0;
+  var toggle = 1;
 
   function switchPress(){
     console.log("toggle" , toggle);
@@ -28,11 +28,13 @@
 
   microgear.on('message', function(topic,data) {
     if(data=="OFF"){
+      document.getElementById("click-door").style.backgroundColor = "#3C5817"
       document.getElementById("doorStatus").innerHTML =  "unlock";
-      document.getElementById("doorImg").src = "images/close-door.png"
+      document.getElementById("doorImg").src = "images/open-door.png"
     }else if(data=="ON"){
+      document.getElementById("click-door").style.backgroundColor = "#824525"
       document.getElementById("doorStatus").innerHTML =  "lock"; 
-      document.getElementById("doorImg").src = "images/open-door.png";
+      document.getElementById("doorImg").src = "images/unlock-door.png";
     }
   });
 
